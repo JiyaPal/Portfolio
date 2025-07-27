ee124c85b6680d7abe4cdeecf1a72d61243224af
JiyaPal <jiya054bteceai22@igdtuw.ac.in>
2025-07-27 13:11:19 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,250 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import colabLogo from './assets/tech_logo/colab.png';
+import scikitLogo from './assets/tech_logo/scikit.png';
+import matlabLogo from './assets/tech_logo/matlab.png';
+import canvaLogo from './assets/tech_logo/canva.png';
+import excelLogo from './assets/tech_logo/excel.png';
+
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/webverse_logo.png';
+import agcLogo from './assets/company_logo/agc_logo.png';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+
+// Education Section Logo's
+import bsaLogo from './assets/education_logo/bsa_logo.png';
+import glaLogo from './assets/education_logo/gla_logo.png';
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/github_det.png';
+import csprepLogo from './assets/work_logo/cs_prep.png';
+import digitLogo from './assets/work_logo/digitrecognizer.png';
+import quantumLogo from './assets/work_logo/quantum.png';
+import prepsterLogo from './assets/work_logo/prepster.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/npm.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      // { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      // { name: 'Angular', logo: angularLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      // { name: 'GSAP', logo: gsapLogo },
+      // { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      // { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      // { name: 'Firebase', logo: firebaseLogo },
+      { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      // { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      // { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Google Colab', logo: colabLogo },
+      { name: 'Scikit-Learn', logo: scikitLogo },
+      { name: 'MATLAB', logo: matlabLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+      { name: 'Canva', logo: canvaLogo },
+      { name: 'Excel', logo: excelLogo },
+    ],
+  },
+  // {
+  //   title: 'Tools',
+  //   skills: [
+  //     { name: 'Git', logo: gitLogo },
+  //     { name: 'GitHub', logo: githubLogo },
+      // { name: 'VS Code', logo: vscodeLogo },
+      // { name: 'Google Colab', logo: colabLogo },
+      // { name: 'Scikit-Learn', logo: scikitLogo },
+      // { name: 'MATLAB', logo: postmanLogo },
+      // { name: 'Canva', logo: mcLogo },
+      // { name: 'Excel', logo: vercelLogo },
+      // { name: 'Netlify', logo: netlifyLogo },
+      // { name: 'Figma', logo: figmaLogo },
+  //   ],
+  // },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: glaLogo,
+      role: "5G Antenna Design Intern",
+      company: "ECE Department, IGDTUW",
+      date: "June 2024 - July 2024",
+      desc: "Completed a six-week internship focused on Augmented Reality (AR) and Virtual Reality (VR), gaining hands-on experience with tools such as A-Frame, Blender, MagicaVoxel, and Unity. Explored the practical implementation of these tools to develop immersive VR projects. Collaborated closely with team members, enhancing communication and team management skills through group-based project development.",
+      skills: [
+        "ANSYS HFSS",
+        "Excel",
+        "Origin Pro",
+        "Microstrip Patch Antenna Design",
+        "S-parameter Analysis",
+        "Z-parameter",
+        "Antenna Theory",
+      ],
+    },
+    {
+      id: 1,
+      img: glaLogo,
+      role: "Metaverse- Virtual Reality Intern",
+      company: "IT Department, IGDTUW",
+      date: "June 2023 - July 2023",
+      desc: "Completed a six-week internship focused on Augmented Reality (AR) and Virtual Reality (VR), gaining hands-on experience with tools such as A-Frame, Blender, MagicaVoxel, and Unity. Explored the practical implementation of these tools to develop immersive VR projects. Collaborated closely with team members, enhancing communication and team management skills through group-based project development.",
+      skills: [
+        "Augmented Reality",
+        "Virtual Reality",
+        "A-frame",
+        "Blender",
+        "Unity",
+        "MagicaVoxel",
+        "Metaverse",
+      ],
+    },
+    // {
+    //   id: 2,
+    //   img: newtonschoolLogo,
+    //   role: "Frontend Intern",
+    //   company: "Newton School",
+    //   date: "September 2021 - August 2022",
+    //   desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+    //   skills: [
+    //     "HTML",
+    //     "CSS",
+    //     "Javascript",
+    //     "Bootstrap",
+    //     "Figma",
+    //     "Material UI",
+    //   ],
+    // },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: glaLogo,
+      school: "Indira Gadhi Delhi Technical University for Women",
+      date: "Nov 2022 - May 2026(Expected)",
+      grade: "6.65 CGPA(till 6th sem)",
+      desc: "I am currently pursuing a Bachelor's degree (B.Tech) in Electronics and Communication Engineering with a specialization in Artificial Intelligence at Indira Gandhi Delhi Technical University for Women. My academic focus is on building a strong foundation in programming, software development, and core computer science principles. As part of my curriculum, I have completed courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Computer Networks. Additionally, I have actively participated in various cultural and technical events, which have significantly enhanced my interpersonal skills and broadened my knowledge base.",
+      degree: "Bachelor of Technology - B.Tech (Electronics and Communication with Artificial Intelligence)",
+    },
+    {
+      id: 1,
+      img: bsaLogo,
+      school: "Rajkiya Pratibha Vikas Vidyalaya, Delhi",
+      date: "April 2020 - Mar 2021",
+      grade: "81.2%",
+      desc: "I completed my class 12 education from RPVV, Surajmal Vihar, under the CBSE board, where I studied Physics, Chemistry, and Mathematics (PCM) along with Economics. Studying Economics alongside the core science subjects provided me with a broader perspective on analytical thinking and decision-making. It also helped me develop a deeper understanding of resource management, logical reasoning, and data interpretation—skills that complement and enhance my technical foundation.",
+      degree: "CBSE(XII) - PCM",
+    },
+    {
+      id: 2,
+      img: bsaLogo,
+      school: "Sarvodaya Kanya Vidyalaya, Delhi",
+      date: "Apr 2018 - March 2019",
+      grade: "92.4%",
+      desc: "I completed my Class 10 education from Sarvodaya Kanya Vidyalaya, Delhi, under the Central Board of Secondary Education (CBSE). My academic curriculum included Science, Social Science, and Mathematics, where I developed a strong foundation and demonstrated a solid command of mathematical concepts. Additionally, I pursued Sanskrit as an additional subject and secured 98% marks, earning the distinction of being the school topper in the subject.",
+      degree: "CBSE(X)",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "PrepsterAI",
+      description:
+        "Built an AI-powered mock interview web app using Next.js, Gemini AI, and Clerk, featuring personalized question generation, secure authentication, and real-time feedback analysis to help users evaluate and improve their responses interactively.",
+      image: prepsterLogo,
+      tags: ["Clerk","Gemini Ai","Tailwind CSS","ReactJS","Drizzle ORM","NextJS","Tailwind CSS"],
+      github: "https://github.com/JiyaPal/PrepsterAI",
+      webapp: "https://prepster-ai-pied.vercel.app/",
+    },
+    {
+      id: 1,
+      title: "Quantum CryptAnalysis",
+      description:
+        "Designed a Python-based QKD simulation framework (BB84, B92) to analyze cryptanalysis under varied noise and eavesdropping conditions. Built an ensemble ML model (Random Forest, Gradient Boosting, XGBoost) achieving 90% accuracy in predicting Eve’s attack success. Utilized Qiskit for quantum simulation and scikit-learn, NumPy, SciPy, and Pandas for data analysis and visualization.",
+      // image: quantumLogo,
+      tags: ["Scikit-learn", "Qiskit", "NumPy", "Pandas", "SciPy"],
+      github: "https://github.com/JiyaPal/Quantum-Cryptanalysis",
+      webapp: "https://github.com/JiyaPal/Quantum-Cryptanalysis",
+    },
+    {
+      id: 2,
+      title: "Write2Recognize",
+      description:
+        "Built a CNN-based handwritten digit recognizer using TensorFlow and Keras to convert hand-written input into machine-readable format with high accuracy.",
+      image: digitLogo,
+      tags: ["Tensorflow", "Keras CNN", "NumPy", "MNIST Dataset", "Matplotlib"],
+      github: "https://github.com/JiyaPal/Write2Recognize",
+      webapp: "https://github.com/JiyaPal/Write2Recognize",
+    },
+  ];  
